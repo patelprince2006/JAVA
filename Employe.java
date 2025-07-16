0b505687dfb69ad8d5fd457dde1dda9b45bc4466
prince <patelprince23906@gmail.com>
2025-07-16 22:55:50 +0530

    initial commit

--- Employe.java
+++ Employe.java
@@ -6,38 +6,6 @@ class Employe{
      private int salary;
      private String status;
 
-// public int getID(){
-//     return id;
-// }
-
-//  public void setID(int id){
-//     this.id=id;
-// }
-
-// public void setName(String Name){
-//     this.Name=Name;
-// }
-
-// public String getName(){
-//     return Name;
-// }
-
-// public String getdepartment(){
-//      return department;
-// }
-
-// public void setdepartment(String department){
-//     this.department=department;
-// }
-
-// public int getsalary(){
-//     return salary;
-// }
-
-// public void setsalary(){
-//     this.salary = salary;
-// }
-
   public Employe(int id,String Name,String department,int salary,String status){
      this.id=id;
      this.Name=Name;
